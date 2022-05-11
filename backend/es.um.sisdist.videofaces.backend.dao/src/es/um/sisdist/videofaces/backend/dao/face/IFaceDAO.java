acdaf61bd1a7bd2faf739de33c38aab82643fcb4
Peter Gabaldon <pedro.gabaldonj@um.es>
2022-05-11 18:52:40 +0200

    DAO Faces

--- backend/es.um.sisdist.videofaces.backend.dao/src/es/um/sisdist/videofaces/backend/dao/face/IFaceDAO.java
+++ backend/es.um.sisdist.videofaces.backend.dao/src/es/um/sisdist/videofaces/backend/dao/face/IFaceDAO.java
@@ -3,15 +3,14 @@ package es.um.sisdist.videofaces.backend.dao.face;
 import java.io.InputStream;
 import java.util.Optional;
 
-import es.um.sisdist.videofaces.backend.dao.models.Video;
-import es.um.sisdist.videofaces.backend.dao.models.Video.PROCESS_STATUS;
+import es.um.sisdist.videofaces.backend.dao.models.Face;
 
 public interface IFaceDAO
 {
-    public Optional<Video> getFaceById(String id);
+    public Optional<Face> getFaceById(String id);
 
     // Get stream of video data
     public InputStream getStreamForFace(String id);
 
-    public Optional<Video> storeFace(String id, String videoid);
+    public Optional<Face> storeFace(String videoid, String filename);
 }
--- backend/es.um.sisdist.videofaces.backend.dao/src/es/um/sisdist/videofaces/backend/dao/face/SQLFaceDAO.java
+++ backend/es.um.sisdist.videofaces.backend.dao/src/es/um/sisdist/videofaces/backend/dao/face/SQLFaceDAO.java
@@ -3,6 +3,7 @@ package es.um.sisdist.videofaces.backend.dao.face;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Blob;
 import java.sql.Connection;
@@ -12,148 +13,94 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Optional;
 
+import es.um.sisdist.videofaces.backend.dao.models.Face;
 import es.um.sisdist.videofaces.backend.dao.models.User;
 import es.um.sisdist.videofaces.backend.dao.models.Video;
 import es.um.sisdist.videofaces.backend.dao.models.Video.PROCESS_STATUS;
 
 public class SQLFaceDAO implements IFaceDAO {
 
-    Connection conn;
+	Connection conn;
 
-    public SQLFaceDAO()
-	{
-        try
-		{
+	public SQLFaceDAO() {
+		try {
 			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-			
+
 			// Si el nombre del host se pasa por environment, se usa aquí.
 			// Si no, se usa localhost. Esto permite configurarlo de forma
 			// sencilla para cuando se ejecute en el contenedor, y a la vez
 			// se pueden hacer pruebas locales
-			Optional<String> sqlServerName = 
-					Optional.ofNullable(System.getenv("SQL_SERVER"));
-			conn = DriverManager.getConnection("jdbc:mysql://" +
-					sqlServerName.orElse("localhost") +
-					"/videofaces?user=root&password=root");
-			
-		} catch (Exception e)
-		{
-			// TODO Auto-generated catch block
+			Optional<String> sqlServerName = Optional.ofNullable(System.getenv("SQL_SERVER"));
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://" + sqlServerName.orElse("localhost") + "/videofaces?user=root&password=root");
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-    }
+	}
 
-
-
-
-    @Override
-    public Optional<Video> getVideoById(String id) {
-        PreparedStatement stm;
-		try
-		{
-			stm = conn.prepareStatement("SELECT * from videos WHERE id = ?");
+	@Override
+	public Optional<Face> getFaceById(String id) {
+		PreparedStatement stm;
+		try {
+			stm = conn.prepareStatement("SELECT * from faces WHERE id = ?");
 			stm.setString(1, id);
 			ResultSet result = stm.executeQuery();
 			if (result.next())
-				return createVideo(result);
-		} catch (SQLException e)
-		{
+				return createFace(result);
+		} catch (SQLException e) {
 			// Fallthrough
 		}
 		return Optional.empty();
-    }
+	}
 
-    @Override
-    public InputStream getStreamForVideo(String id) {
-        PreparedStatement stm;
-		try
-		{
-			stm = conn.prepareStatement("SELECT videodata from videos WHERE id = ?");
+	@Override
+	public InputStream getStreamForFace(String id) {
+		PreparedStatement stm;
+		try {
+			stm = conn.prepareStatement("SELECT imagedata from faces WHERE id = ?");
 			stm.setString(1, id);
 			ResultSet result = stm.executeQuery();
 			Blob objetoblob = result.getBlob(1);
-            InputStream resultado =  objetoblob.getBinaryStream();
-            return resultado;
+			InputStream resultado = objetoblob.getBinaryStream();
+			return resultado;
 
-            
-		} catch (SQLException e)
-		{
+		} catch (SQLException e) {
 			// Fallthrough
 		}
 		return null;
-    }
+	}
 
-    @Override
-    public PROCESS_STATUS getVideoStatus(String id) {
-        PreparedStatement stm;
-		try
-		{
-			stm = conn.prepareStatement("SELECT process_status from videos WHERE id = ?");
-			stm.setString(1, id);
-			ResultSet result = stm.executeQuery();
-			if(result.getInt(1) == 0){
-                return Video.PROCESS_STATUS.PROCESSING;
-            }
-            else{
-                return Video.PROCESS_STATUS.PROCESSED;
-            }
-		} catch (SQLException e)
-		{
-			// Fallthrough
-		}
-		return Video.PROCESS_STATUS.PROCESSING;
-    }
-
-    public Optional<Video> storeVideo(String userid, PROCESS_STATUS pstatus, String date, String filename)
-    {
-        PreparedStatement stm;
-        try
-        {
-			String id = User.md5pass(filename+userid);
-            stm = conn.prepareStatement("INSERT INTO videos VALUES (?,?,?,?,?,?)");
-            stm.setString(1, id);
-            stm.setString(2, userid);
-            stm.setInt(3, pstatus.ordinal());
-            stm.setString(4, date);
-            stm.setString(5, filename);
-
-            File file = new File("/tmp/output");
+	public Optional<Face> storeFace(String videoid, String filename) {
+		PreparedStatement stm;
+		try {
+			File file = new File(filename);
 			FileInputStream inputStream = new FileInputStream(file);
-            stm.setBlob(6, inputStream);
-            int row = stm.executeUpdate();
-            if (row == 1)
-                return this.getVideoById(id);
-        } catch (SQLException | FileNotFoundException e)
-        {
-            // Fallthrough
-        }
-        return Optional.empty();
-    }
 
-    private Optional<Video> createVideo(ResultSet result)
-	{
+			String id = User.md5pass(videoid + (new String(inputStream.readNBytes(50))));
+			stm = conn.prepareStatement("INSERT INTO faces VALUES (?,?,?)");
+			stm.setString(1, id);
+			stm.setString(2, videoid);
+			stm.setBlob(3, inputStream);
+			int row = stm.executeUpdate();
+			if (row == 1)
+				return this.getFaceById(id);
+		} catch (SQLException | FileNotFoundException e) {
+			// Fallthrough
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return Optional.empty();
+	}
 
+	private Optional<Face> createFace(ResultSet result) {
 
-        // String id, String userid, PROCESS_STATUS pstatus, String date, String filename
-		try
-		{
-            if(result.getInt(3) == 1){
-                return Optional.of(new Video(result.getString(1), // id
-					result.getString(2), // userid
-                    Video.PROCESS_STATUS.PROCESSED,  // status
-					result.getString(3), // filename
-					result.getString(4))); // name
-            }
-            else{
-                return Optional.of(new Video(result.getString(1), // id
-					result.getString(2), // userid
-                    Video.PROCESS_STATUS.PROCESSING,  // status
-					result.getString(3), // filename
-					result.getString(4))); // name
-
-            }
-		} catch (SQLException e)
-		{
+		// String id, String userid, PROCESS_STATUS pstatus, String date, String
+		// filename
+		try {
+			return Optional.of(new Face(result.getString(1), // id
+					result.getString(2))); // email
+		} catch (SQLException e) {
 			return Optional.empty();
 		}
 	}
